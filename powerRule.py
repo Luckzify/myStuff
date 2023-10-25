217951ebafc02d6415816974f7a8e7b765f1def1
Luckzify <144089759+Luckzify@users.noreply.github.com>
2023-10-24 21:46:23 -0400

    Add files via upload

---
+++ powerRule.py
@@ -0,0 +1,52 @@
+#Gavin Brady
+#Oct 24 2023
+#Finds derivative of polynomials using power rule
+
+while True:
+    function = input('Enter function, f(x) = ') #gets function to be differentiated
+    termsList = function.split(" + ")#splits function into list of all terms
+    
+    derivative = '' 
+    
+    for term in termsList:#goes term by term through list
+        
+        if term.find('x^') > 0:#if term has coefficient and exponent, finds new coefficient and power
+            coeff = int(term[:term.find('x')]) * int(term[(term.find('^')+1):])
+            power = int(term[(term.find('^')+1):]) - 1
+            
+        elif term.find('x^') == 0: #solves if term has no coefficient
+            coeff = int(term[(term.find('^')+1):])
+            power = int(term[(term.find('^')+1):]) - 1
+            
+        elif term.find("^") == -1: #solves if no power
+            if term.find('x') == 0: #solves if term has no power and no coefficient
+                coeff = 1
+                power = 0
+            if term.find('x') > 0: #solves if term has no power but has coefficient
+                coeff = int(term[:term.find('x')])
+                power = 0
+            if term.find('x') == -1: #solves if term is constant
+                coeff = 0
+                power = 0
+                
+        #puts differentiated term together
+        diffTerm = f"{coeff}x^{power}"
+
+        if diffTerm == "0x^0":
+            diffTerm = ''
+        elif diffTerm.find('x^0') != -1:
+            diffTerm = diffTerm[:diffTerm.find('x^0')]
+
+        if termsList.index(term) == 0 or diffTerm == '': #adds with no " + " before in case of first term, or does not add at all in case of blank term
+            derivative += diffTerm
+        else: #adds term with " + " prefacing
+            derivative += f" + {diffTerm}"
+
+    print(f"The derivative is, f'(x) = {derivative}")
+        
+            
+    
+                    
+                    
+                    
+                    
